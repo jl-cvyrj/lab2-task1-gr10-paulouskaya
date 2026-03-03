3f5e431c983eae1fb6f1be729694c4f22f126423
Karalina Paulouskaya <55511640-karalina-p@users.noreply.replit.com>
2026-03-03 15:46:47 +0000

    add palindrome module interface

---
+++ task1_var31/include/palindrome.h
@@ -0,0 +1,18 @@
+/* Аўтар: Караліна Паўлоўская */
+
+#ifndef PALINDROME_H
+
+/*
+ * palindrome.h
+ *
+ * Інтэрфейс модуля праверкі натуральнага ліку
+ * на паліндром.
+ */
+ 
+#define PALINDROME_H
+#include <stdbool.h>
+
+bool
+is_palindrome(unsigned int number);
+
+#endif /* PALINDROME_H */
